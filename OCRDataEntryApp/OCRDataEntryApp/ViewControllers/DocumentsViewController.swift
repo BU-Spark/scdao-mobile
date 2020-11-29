d69fa2d73fabf5fd9e8a5124a9a2f60d3206b51b
Victor <32070827+victorf-github@users.noreply.github.com>
2020-11-29 10:51:09 -0500

    Intial Commit for Documents Page

---
+++ OCRDataEntryApp/OCRDataEntryApp/ViewControllers/DocumentsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DocumentsViewController.swift
+//  OCRDataEntryApp
+//
+//  Created by Victor Figueroa on 11/11/20.
+//  Copyright © 2020 SparkBU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DocumentsViewController: UIViewController {
+    
+    @IBAction func CriminalCompliantFormIsClicked(_ sender: Any) {
+        //Make API request that this form is has been selected, then open the scanner
+    }
+    @IBAction func ApplicationForCriminalComplaintFormIsClicked(_ sender: Any) {
+        //Make API request that this form is has been selected, then open the scanner
+    }
+    @IBAction func IncidentReportFormIsClicked(_ sender: Any) {
+        //Make API request that this form is has been selected, then open the scanner
+    }
+    @IBAction func ArrestBookingFormIsClicked(_ sender: Any) {
+        //Make API request that this form is has been selected, then open the scanner
+    }
+    @IBAction func MirandaFormIsClicked(_ sender: Any) {
+        //Make API request that this form is has been selected, then open the scanner
+    }
+    @IBAction func ProbationRecordFormIsClicked(_ sender: Any) {
+        //Make API request that this form is has been selected, then open the scanner
+    }
+    
+    private func toHome() {
+        let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
+    }
+}
