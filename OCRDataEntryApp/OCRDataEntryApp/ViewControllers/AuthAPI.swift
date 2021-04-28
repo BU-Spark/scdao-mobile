1435596e5b660120e732d9ca0169fd7f5355b5b6
Justin <justinjanice123@gmail.com>
2021-04-27 23:16:45 -0400

    delete duplicate code

--- OCRDataEntryApp/OCRDataEntryApp/ViewControllers/AuthAPI.swift
+++ OCRDataEntryApp/OCRDataEntryApp/ViewControllers/AuthAPI.swift
@@ -126,45 +126,4 @@ struct AuthAPI {
             completion(nil, .ValidationError, "" )
         }
     }
-    
-    
-    private func responseString(endpoint: String, data: [String: String], completion: @escaping(Data?, AUTHError?, String) -> Void) {
-        let fullURL = baseURL.appendingPathComponent(endpoint)
-        
-        var urlRequest = URLRequest(url: fullURL)
-        
-        urlRequest.httpMethod = "POST"
-        urlRequest.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        do {
-            // "username=111&password=3333"
-            
-            let body = data.map { $0 + "=" + $1 }.joined(separator: "&")
-            let bodyData = body.data(using: .utf8, allowLossyConversion: false)
-            
-            urlRequest.httpBody = bodyData
-            
-            print("Request: " + fullURL.absoluteString)
-            print("Body: " + body)
-            
-            URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-                if data == nil {
-                    completion(nil, .HTTPValidationError, "Backend not running")
-                    return
-                }
-                let plainResponse = String(data: (data)!, encoding: .utf8)
-                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-                    completion(nil, .HTTPValidationError, plainResponse!)
-                    return
-                }
-                
-                completion(data, nil, plainResponse!)
-            }
-    
-            .resume()
-        } catch {
-            completion(nil, .ValidationError, "" )
-        }
-    }
 }
