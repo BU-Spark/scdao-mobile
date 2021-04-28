d435a405202a6acfec4b078e2f31d1f69f00e851
nikitajakkam <njakkam@bu.edu>
2021-04-28 02:41:59 -0400

    updating upload status code - statusHandler() function now called from UploadViewController() - app now alerts users if they are unauthorized to upload documents - alerts users if a server error occurs during image processing - loading alert is only displayed once while document is being processed - updated comments

--- OCRDataEntryApp/OCRDataEntryApp/ViewControllers/ImageApi.swift
+++ OCRDataEntryApp/OCRDataEntryApp/ViewControllers/ImageApi.swift
@@ -20,9 +20,6 @@ class ImageAPI {
     let baseURL: URL
     let token: String
     let type: String
-    private var timer: Timer?
-    private var statusCheckedFirst = false
-    private var timeout = 40 //number of API calls until timeout
     
     init(baseURL: String, token: String, type: String) {
         let resourceString = "http://\(baseURL)/api/"
@@ -34,8 +31,8 @@ class ImageAPI {
         self.type = type
     }
     
-    // uploads image
-    func uploadImage(imageToUpload: UIImage, completion: @escaping (Bool?)->()){
+    // uploads image and gets back job_id
+    func uploadImage(imageToUpload: UIImage, completion: @escaping (String?)->()){
         let myUrl = baseURL.appendingPathComponent("v1/uploads/ccf")
         let request = NSMutableURLRequest(url:myUrl)
         
@@ -43,7 +40,6 @@ class ImageAPI {
         
         let param: [String: String] = [:]
         let boundary = generateBoundaryString()
-        
         
         request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
@@ -58,25 +54,19 @@ class ImageAPI {
         URLSession.shared.dataTask(with: request as URLRequest) { [self]
             data, response, error in
             if response == nil{
-                print("Error: Not connected to backend")
-                return completion(nil)
+                return completion("Not connected to API")
             }
             if let serverResponse = response as? HTTPURLResponse, !(200...299).contains(serverResponse.statusCode) {
-                serverErrorHandler(response: response as! HTTPURLResponse) // handle status codes with errors
-                return completion(nil)
+                return(completion(serverErrorHandler(response: response as! HTTPURLResponse))) // handle status codes with errors
             }
             if error != nil || data == nil {
-                print("Error: ", error.debugDescription)
+                print("Error ", error?.localizedDescription ?? "")
                 return completion(nil)
             }
             if let data = data{
                 let json = try! JSONSerialization.jsonObject(with: data, options: [])
                 if let responseString = json as? [String: Any], let job_id = responseString["job id"] as? String {
-                    DispatchQueue.main.async{
-                        //timer calls statusHandler() function repeatedly until task state returns a Success or Fail
-                        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(statusHandler(sender:)), userInfo: (job_id), repeats: true)
-                    }
-                    return completion(true)
+                    return completion(job_id)
                 }
                 return completion(nil)
             }
@@ -87,8 +77,8 @@ class ImageAPI {
     }
     
     // retrieves upload status (SUCCESS, AWS_FAIL, OCR_FAIL, STARTING)
-    @objc private func uploadStatus (task_id: String, completion: @escaping (String?)->()){
-        let myUrl = baseURL.appendingPathComponent("v1/tasks/\(task_id)") //add task id
+    @objc func uploadStatus (task_id: String, completion: @escaping (String?)->()){
+        let myUrl = baseURL.appendingPathComponent("v1/tasks/\(task_id)")
         let request = NSMutableURLRequest(url:myUrl)
         
         request.httpMethod = "GET";
@@ -101,10 +91,11 @@ class ImageAPI {
                 return completion(nil)
             }
             if let serverResponse = response as? HTTPURLResponse, !(200...299).contains(serverResponse.statusCode) {
+                print("Error Code: ", (completion(self.serverErrorHandler(response: response as! HTTPURLResponse))))
                 return completion(nil)
             }
             if error != nil || data == nil {
-                print("Error: ", error.debugDescription)
+                print("Error ", error?.localizedDescription ?? "")
                 return completion(nil)
             }
             if let data = data{
@@ -121,81 +112,18 @@ class ImageAPI {
         }.resume()
     }
     
-    // called by timer to fetch upload status until SUCCESS or FAIL (timer invalidates upon SUCCESS or FAIL)
-    @objc private func statusHandler(sender: Timer) {
-        let jobID = sender.userInfo
-        timeout = timeout - 1
-        if (statusCheckedFirst == true){ //if loading alert already displayed
-            DispatchQueue.main.async {
-                getTopMostViewController()?.dismiss(animated: false, completion: nil) //remove loading alert
-            }
-        }
-        statusCheckedFirst = true
-        
-        uploadStatus(task_id: jobID as! String){ status in // get status using job_id
-            print(self.timeout)
-            if let status = status{
-                print("STATUS: ", status)
-                if (status == "SUCCESS"){
-                    self.statusCheckedFirst = false
-                    self.timer?.invalidate()
-                    self.timer = nil
-                    self.timeout = 40 //reset number of API calls until timeout to 20
-                    DispatchQueue.main.async {
-                        showAlert(field: "Upload Successful", info: "Document uploaded successfully.")
-                    }
-                }
-                if (status == "AWS_FAIL" || status == "OCR_FAIL"){
-                    self.statusCheckedFirst = false
-                    self.timer?.invalidate()
-                    self.timer = nil
-                    self.timeout = 40
-                    DispatchQueue.main.async {
-                        showAlert(field: "Upload Failed", info: "Document upload failed. Please try again.")
-                    }
-                }
-                if (status == "STARTING"){
-                    if (self.timeout == 0){
-                        self.statusCheckedFirst = false
-                        self.timer?.invalidate()
-                        self.timer = nil
-                        self.timeout = 40
-                        print("error here")
-                        DispatchQueue.main.async {
-                            showAlert(field: "Upload Timeout", info: "Please try again.")
-                        }
-                    }
-                    else{
-                        DispatchQueue.main.async {
-                            pendingAlert(info: "Uploading Image ...") //display loading alert
-                        }
-                    }
-                }
-            }
-            if (self.timeout == 0){
-                self.timeout = 40
-                DispatchQueue.main.async {
-                  showAlert(field: "Upload Timeout", info: "Please try again.")
-                }
-            }
-        }
-        print("Error getting image upload status")
-    }
-    
-    @objc private func serverErrorHandler(response: HTTPURLResponse){
+    // returns API response codes for specific errors
+    @objc private func serverErrorHandler(response: HTTPURLResponse) -> String{
         let statusCode = response.statusCode
         switch statusCode{
         case 400:
-            showAlert(field: "400 Error", info: "Please upload a document with the valid parameters")
-            print("400: File param error")
+            return "Invalid Params"
             
         case 403:
-            showAlert(field: "403 Forbidden", info: "User not authorized to upload documents.")
-            print("403: Forbidden")
+            return "Unauthorized Access"
             
         default:
-            showAlert(field: "Server Error", info: "Please try again.")
-            print("Server Error")
+            return "Server Error"
         }
     }
 }
@@ -218,38 +146,6 @@ func createBodyWithParameters(parameters: [String: String]?, filePathKey: String
 
 func generateBoundaryString() -> String {
     return "Boundary-\(NSUUID().uuidString)"
-}
-
-//  Code borrowed from: https://www.javaer101.com/en/article/11613040.html (Published 10/19/2020)
-func getTopMostViewController() -> UIViewController? {
-    var topMostViewController = UIApplication.shared.keyWindow?.rootViewController
-    
-    while let presentedViewController = topMostViewController?.presentedViewController {
-        topMostViewController = presentedViewController
-    }
-    //    print("VC: ", topMostViewController)
-    return topMostViewController
-}
-
-func showAlert(field: String, info: String) {
-    let alert = UIAlertController(title: field, message: info, preferredStyle: .alert)
-    
-    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-    
-    getTopMostViewController()?.present(alert, animated: false, completion: nil) 
-}
-
-//  Code borrowed from: https://stackoverflow.com/questions/27960556/loading-an-overlay-when-running-long-tasks-in-ios (Published 12/14/15 by user Ajinkya Patil)
-func pendingAlert(info: String) {
-    let pending = UIAlertController(title: nil, message: info, preferredStyle: .alert) //loading alert
-    
-    let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
-    loadingIndicator.hidesWhenStopped = true
-    loadingIndicator.style = UIActivityIndicatorView.Style.medium
-    loadingIndicator.startAnimating();
-    
-    pending.view.addSubview(loadingIndicator)
-    getTopMostViewController()?.present(pending, animated: false, completion: nil)
 }
 
 extension NSMutableData {
--- OCRDataEntryApp/OCRDataEntryApp/ViewControllers/UploadViewController.swift
+++ OCRDataEntryApp/OCRDataEntryApp/ViewControllers/UploadViewController.swift
@@ -4,6 +4,8 @@
 //  Created by Artemis Chen on 12/1/20.
 //  Copyright © 2020 SparkBU. All rights reserved.
 //  resource:https://www.youtube.com/watch?v=hg-6sOOxeHA
+//  Loading Indicator/Pending alert code borrowed from:
+//      https://stackoverflow.com/questions/27960556/loading-an-overlay-when-running-long-tasks-in-ios (Published 12/14/15 by user Ajinkya Patil)
 
 import SwiftUI
 import UIKit
@@ -11,23 +13,19 @@ import UIKit
 final class UploadViewController: UIViewController {
     @IBOutlet
     var imageView: UIImageView!
+    var timer: Timer?
+    let pending = UIAlertController(title: nil, message: "Uploading Image...", preferredStyle: .alert) //loading alert
+    let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+    let maxAPICalls = 30 //max number of API calls to check upload status
+    var apiCallCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     @IBAction func didTapCameraButton(){
         #if !targetEnvironment(simulator)
         var button = sender as UIButton
-        
-        //        if sender.isOn {
-        //                    button.isOn = true  //   MyButton(sender: UIBarButtonItem) = true
-        //                }
-        //                else {
-        //                   button.isOn = false  //  MyButton(sender: UIBarButtonItem) = false
-        //                }
-        //
         let vc = UIImagePickerController()
         vc.sourceType = .camera
         vc.delegate = self
@@ -50,11 +48,6 @@ final class UploadViewController: UIViewController {
     @IBAction func didTapUploadButton(){
         let vc = UIImagePickerController()
         present(vc, animated: true, completion: nil )
-        
-        //showAlertButtonTapped()
-        //send http request here
-        //let apiCall = ImageAPI(baseURL: "localhost")
-        //apiCall.uploadImage(imageToUpload: imageView.image)
     }
     
 }
@@ -74,19 +67,130 @@ extension UploadViewController: UIImagePickerControllerDelegate,UINavigationCont
             
             let apiCall = ImageAPI(baseURL: Config.baseURL, token: token, type: type)
             
-            let _: () = apiCall.uploadImage(imageToUpload: imageView.image!) { didUpload in
+            let _: () = apiCall.uploadImage(imageToUpload: imageView.image!) { status in //
+                if (status == nil){
+                    DispatchQueue.main.async {
+                        self.showAlert(field: "Error Uploading Image", info: "Please try again.")
+                    }
+                    return
+                }
+                if (status == "Unauthorized Access"){
+                    DispatchQueue.main.async {
+                        self.showAlert(field: "Unauthorized Access", info: "You do not have authorization to upload documents.")
+                    }
+                    return
+                }
+                if (status == "Invalid Params"){
+                    DispatchQueue.main.async {
+                        self.showAlert(field: "Invalid File Upload", info: "Uploaded file does not match the valid parameters.")
+                    }
+                    return
+                }
+                if (status == "Server Error"){
+                    DispatchQueue.main.async {
+                        self.showAlert(field: "Server Error", info: "")
+                    }
+                    return
+                }
                 
-                if let didUpload = didUpload{
-                    print("Uploaded", didUpload)
+                else{
+                    print("JobID:", status!)
+                    DispatchQueue.main.async {
+                        self.loadingIndicator.hidesWhenStopped = true
+                        self.loadingIndicator.style = UIActivityIndicatorView.Style.medium
+                        self.loadingIndicator.startAnimating();
+                        
+                        self.pending.view.addSubview(self.loadingIndicator)
+                        self.present(self.pending, animated: false, completion: nil)
+                        //timer function calls statusHandler() repeatedly until upload returns SUCCESS, FAIL, or max number of requests exceeded
+                        self.timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.statusHandler(sender:)), userInfo: status, repeats: true)
+                    }
                 }
             }
             
         } 
     }
     
+    // called by timer to fetch upload status until SUCCESS or FAIL (timer invalidates upon SUCCESS or FAIL)
+    @objc private func statusHandler(sender: Timer) {
+        guard let token = UserDefaults.standard.string(forKey: "tokenValue"),
+              let type = UserDefaults.standard.string(forKey: "tokenType") else {
+            return
+        }
+        
+        let jobID = sender.userInfo
+        let apiCall = ImageAPI(baseURL: Config.baseURL, token: token, type: type)
+        apiCallCount += 1
+        
+        apiCall.uploadStatus(task_id: jobID as! String){ status in // get status using job_id
+            if let status = status{
+                print("STATUS: ", status)
+                if (status == "SUCCESS"){
+                    self.timer?.invalidate()
+                    self.timer = nil
+                    self.apiCallCount = 0
+                    DispatchQueue.main.async {
+                        self.pending.dismiss(animated: false, completion:{
+                            self.showAlert(field: "Upload Successful", info: "Document uploaded successfully")
+                        })
+                    }
+                }
+                
+                if (status == "AWS_FAIL"){
+                    self.timer?.invalidate()
+                    self.timer = nil
+                    self.apiCallCount = 0
+                    DispatchQueue.main.async {
+                        self.pending.dismiss(animated: false, completion:{
+                            self.showAlert(field: "Upload Failed", info: "Document upload failed. Please try again.")
+                        })
+                    }
+                }
+                if (status == "OCR_FAIL"){
+                    self.timer?.invalidate()
+                    self.timer = nil
+                    self.apiCallCount = 0
+                    DispatchQueue.main.async {
+                        self.pending.dismiss(animated: false, completion:{
+                            self.showAlert(field: "Document Processing Failed", info: "Please try again.")
+                        })
+                    }
+                }
+                else{
+                    if (self.apiCallCount == self.maxAPICalls){
+                        self.timer?.invalidate()
+                        self.timer = nil 
+                        self.apiCallCount = 0
+                        DispatchQueue.main.async {
+                            self.pending.dismiss(animated: false, completion:{
+                                self.showAlert(field: "Upload Timed Out", info: "Please try again.")
+                            })
+                        }
+                    }
+                }
+            }
+            else{
+                self.timer?.invalidate()
+                self.timer = nil
+                self.apiCallCount = 0
+                DispatchQueue.main.async {
+                    self.pending.dismiss(animated: false, completion:{
+                        self.showAlert(field: "Server Error", info: "")
+                    })
+                }
+                
+            }
+            
+        }
+    }
+    
+    func showAlert(field: String, info: String) {
+        let alert = UIAlertController(title: field, message: info, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: false, completion: nil)
+    }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
 }
-
