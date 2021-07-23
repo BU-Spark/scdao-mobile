d89fa5e8385a61afa7c5b54c0f20922a680d0733
jin-jeffrey <jinjeff2@gmail.com>
2021-07-23 17:32:10 -0400

    connected login and signup to backend, need to add data validation

--- SCDAO-Mobile/SCDAO-Mobile/ViewControllers/Login.swift
+++ SCDAO-Mobile/SCDAO-Mobile/ViewControllers/Login.swift
@@ -6,16 +6,22 @@
 //
 
 import UIKit
+import Alamofire
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var userTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var forgotPasswordButton: UIButton!
-    @IBOutlet weak var userView: UIView!
-    @IBOutlet weak var passView: UIView!
-    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var forgotPasswordButton: UIButton!
+    @IBOutlet private weak var userView: UIView!
+    @IBOutlet private weak var passView: UIView!
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var errorLabel: UILabel!
     
+    private var minPasswordLength: Int = 8
+    private var email: String = ""
+    private var password: String = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,9 +30,53 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func signUpButton(_ sender: Any) {
-//        let mainStoryBoard : UIStoryboard = UIStoryboard(name: "main", bundle: nil)
-//        let signUpView = mainStoryBoard.instantiateViewController(identifier: "SignupViewController") as! SignupViewController
-//        self.present(signUpView, animated: true, completion: nil)
         performSegue(withIdentifier: "loginToSignup", sender: nil)
+    }
+    
+    private func isPasswordValid(_ password : String) -> Bool{
+        let passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{" + String(minPasswordLength) + ",}$"
+        return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
+    }
+    
+    private func isEmailValid(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: email)
+    }
+    
+
+    @IBAction func onEmailTextChanged(_ textField: UITextField) {
+        self.email = textField.text ?? ""
+    }
+
+    @IBAction func onPasswordTextChanged(_ textField: UITextField) {
+        self.password = textField.text ?? ""
+    }
+    
+    private func validate() -> Bool {
+//        guard isEmailValid(this.email)
+        return true
+    }
+    
+    //check login credentials and create authentication token to login if successful
+    @IBAction func loginPressed(_ button: UIButton) {
+        // setup parameters and request URL
+        let params: [String: Any] = ["username": self.email, "password": self.password]
+        let loginRoute = Config.baseURL + "/api/token"
+        
+        print(params)
+        
+        AF.request(loginRoute, method: .post, parameters: params, encoding: URLEncoding.httpBody, headers: nil)
+            .responseJSON{
+                response in
+                do {
+                    //
+                    debugPrint(response)
+                } catch {
+                    
+                }
+                    
+            }
+        
     }
 }
--- SCDAO-Mobile/SCDAO-Mobile/ViewControllers/Signup.swift
+++ SCDAO-Mobile/SCDAO-Mobile/ViewControllers/Signup.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class SignupViewController: UIViewController {
     
@@ -15,11 +16,47 @@ class SignupViewController: UIViewController {
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signupButton: UIButton!
+    
+    private var email: String = ""
+    private var user: String = ""
+    private var password: String = ""
+    private var passwordConf: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Setup view
         signupButton.layer.cornerRadius = 10
 
+    }
+    
+    @IBAction func onEmailTextChanged(_ textField: UITextField) {
+        self.email = textField.text ?? ""
+    }
+    @IBAction func onUserTextChanged(_ textField: UITextField) {
+        self.user = textField.text ?? ""
+    }
+    @IBAction func onPasswordTextChanged(_ textField: UITextField) {
+        self.password = textField.text ?? ""
+    }
+    @IBAction func onPasswordConfTextChanged(_ textField: UITextField) {
+        self.passwordConf = textField.text ?? ""
+    }
+    @IBAction func onSignupPressed(_ sender: UIButton) {
+
+        let params: [String: Any] = ["username": self.email, "password": self.password]
+        
+        let signupRoute = Config.baseURL + "/api/signup"
+        
+        AF.request(signupRoute, method: .post, parameters: params, encoding: URLEncoding.httpBody, headers: nil)
+            .responseJSON {
+                response in
+                do {
+                    debugPrint(response)
+                } catch {
+                    
+                }
+            }
+        
     }
 }
